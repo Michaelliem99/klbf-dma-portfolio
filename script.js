971b8f6fff64f4c04b227917936432e0804b5059
michaelianto <61343284+michaelianto@users.noreply.github.com>
2022-08-01 22:56:58 +0700

    Add files via upload
    Toggle Function to Show and Hide Projects in Project Page

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ $(function(){
 		inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
 	});
 });
+
+
+
+$('input[type=radio]').click(function(e){
+	var radioValue = $("input[name='project']:checked").val();
+	var containerDescriptive = document.getElementById('container-descriptive');
+	var containerPredictive = document.getElementById('container-predictive');
+	if(radioValue == 'descriptive'){
+		containerDescriptive.style.display = 'flex'
+		containerPredictive.style.display = 'none';
+	}else if(radioValue == 'diagnostic'){
+	
+	}else if(radioValue == 'predictive'){
+		containerPredictive.style.display = 'flex'
+		containerDescriptive.style.display = 'none';
+	}else if(radioValue == 'prescriptive'){
+	
+	}else if(radioValue == 'preemptive'){
+	
+	}
+})
+
