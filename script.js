e0e929b8067c51ef4aa9ce61e7f603d57614da54
michaelianto <61343284+michaelianto@users.noreply.github.com>
2022-08-11 08:40:16 +0700

    Add files via upload
    Revision in Navbar and Project Page

--- script.js
+++ script.js
@@ -17,18 +17,23 @@ $('input[type=radio]').click(function(e){
 	var radioValue = $("input[name='project']:checked").val();
 	var containerDescriptive = document.getElementById('container-descriptive');
 	var containerPredictive = document.getElementById('container-predictive');
+
+	var title = $('#project-category-title');
+
 	if(radioValue == 'descriptive'){
 		containerDescriptive.style.display = 'flex'
 		containerPredictive.style.display = 'none';
+		title.text("Descriptive Analytics Use Case");
 	}else if(radioValue == 'diagnostic'){
-	
+		title.text("Diagnostic Analytics Use Case");
 	}else if(radioValue == 'predictive'){
 		containerPredictive.style.display = 'flex'
 		containerDescriptive.style.display = 'none';
+		title.text("Predictive Analytics Use Case");
 	}else if(radioValue == 'prescriptive'){
-	
+		title.text("Prescriptive Analytics Use Case");
 	}else if(radioValue == 'preemptive'){
-	
+		title.text("Preemptive Analytics Use Case");
 	}
 })
 
